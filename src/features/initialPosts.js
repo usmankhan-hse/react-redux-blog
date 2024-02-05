cbde8486e21a82dfb23dc8e642c67a3e9ffc5e97
usmankhan-hse <usmankhan.hse@gmail.com>
2024-02-05 17:47:07 +0400

    updated

---
+++ src/features/initialPosts.js
@@ -0,0 +1,25 @@
+//SAMPLE POSTS
+
+const initialState = {
+    postState: [
+        {
+            id: 1,
+            author: "usman",
+            content: "Hi my name is usman i am an it expert."
+        },
+        {
+            id: 2,
+            author: "umer",
+            content: "Hi my name is umer, i am a civil engineer"
+        },
+        {
+            id: 3,
+            author: "ali",
+            content: "Hi my name is ali i am a doctor."
+        }
+    ]
+}
+
+
+
+
